4f22620fa6cbc42eac8c7d3dd7ed019243b88601
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-21 01:17:14 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Osinachi',
+    description: 'Osinachi Chukwujama Portfolio',
+    keywords: 'osinachi, gatsbyjs, gatsby, javascript',
+    siteUrl: 'http://osinachi.me',
+    author: {
+      name: 'Osinachi Chukwujama',
+      url: 'https://twitter.com/vicradon',
+      email: 'vicraph7@gmail.com'
+    }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem'
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require('tailwindcss'),
+          require('./tailwind.config.js') // Optional: Load custom Tailwind CSS configuration
+        ]
+      }
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet'
+  ]
+}
